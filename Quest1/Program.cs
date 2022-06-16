002937a6e3857e49298ea21ace29f400828f0d48
Nord.A <mr.nordakov@gmail.com>
2022-06-16 17:06:22 +0300

    add all

---
+++ Quest1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99999 || number < 10000) Console.WriteLine("Введите пятизначное число");
+else {
+    int n0 = number / 10000;
+    //Console.WriteLine(n0);
+    int n1 = (number % 10000) / 1000;
+    //Console.WriteLine(n1);
+    int n3 = (number % 100) / 10;
+    //Console.WriteLine(n3);
+    int n4 = number % 10;
+    //Console.WriteLine(n4);
+    if (n0 == n4 && n1 == n3) Console.WriteLine($"Число {number} - Полиндром");
+    else Console.WriteLine($"Число {number} не является полиндромом");
+}
---
+++ Quest2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты для двух точек 
+// и находит растояние между ними в 3D пространстве.
+
+void Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+   double distance = Math.Sqrt(Math.Pow((x2 - x1),2) + Math.Pow((y2 - y1),2) + Math.Pow((z2 - z1),2));
+   Console.WriteLine($"Растояние между точками {distance}");  
+}
+
+Console.WriteLine("Введите координаты X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Z первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Z второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Distance3D(x1, y1, z1, x2, y2, z2);
