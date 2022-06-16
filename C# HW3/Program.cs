ee5ac48c4408bec296627a89fe1359f6d61714c2
Naganishe <mr.nordakov@mail.ru>
2022-06-17 00:23:20 +0300

    add C# HW3

---
+++ C# HW3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу кубов чисел от 1 до N.
+
+void Cube(int number)
+{
+    int cubeI = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        cubeI = i*i*i;
+        Console.WriteLine($"Числа в кубе до {number}: {cubeI}");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Cube(number);
